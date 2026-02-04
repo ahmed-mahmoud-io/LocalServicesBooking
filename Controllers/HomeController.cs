28ef8e6a6dc8f155aab2565fe63e3bf22db912ff
ahmedmhmoud1712 <ahmedmhmoud1712@users.noreply.github.com>
2026-02-04 02:14:03 +0200

    Config: Add HTTPS rewrite rule to web.config

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,11 +33,6 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult DesignShowcase()
-    {
-        return View();
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
